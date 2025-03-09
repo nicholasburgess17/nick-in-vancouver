3158664a06e0f12bf86ac4af3f86b0297ab91a00
nicholasburgess17 <nicholas.burgess13@gmail.com>
2025-03-09 16:03:22 -0700

    cahnge Navbar to Header

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
-//import Home from "./pages/Home";
-//import Portfolio from "./pages/Portfolio";
-import Store from "./pages/Store";
-import ProductPage from "./pages/ProductPage";
-import Cart from "./pages/Cart";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+
 
 const App = () => {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
-        <Navbar />
+        <Header />
         <main className="flex-grow pt-16">
           <Routes>
             <Route path="/" element={<Home />} />
