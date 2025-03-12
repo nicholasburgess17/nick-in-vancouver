8142a65f569c2b211020cbb054f1cd30d616d1c6
nicholasburgess17 <nicholas.burgess13@gmail.com>
2025-03-11 19:40:13 -0700

    Remove .env from repository

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 
 // Your Firebase Config (Find it in Firebase Console -> Project Settings)
 const firebaseConfig = {
-  apiKey: "AIzaSyDiqRJ0ivmhV3NqS--yuDF129mo365tbwU",
-  authDomain: "NickPhotoPortfolio.firebaseapp.com",
-  projectId: "nickphotoportfolio",
-  storageBucket: "NickPhotoPortfolio.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
