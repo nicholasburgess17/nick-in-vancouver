6f611bb62b3280c9767abbe823d5c4ba780f9a12
nicholasburgess17 <nicholas.burgess13@gmail.com>
2025-03-11 18:55:40 -0700

    enable google auth

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -1,7 +1,8 @@
 // Import Firebase modules
 import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your Firebase Config (Find it in Firebase Console -> Project Settings)
 const firebaseConfig = {
@@ -12,10 +13,12 @@ const firebaseConfig = {
   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
   appId: "YOUR_APP_ID",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const storage = getStorage(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider(); // Google Provider
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { storage, db };
+export { auth, provider, db, storage };
+
